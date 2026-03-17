99bd1c3485c78a8c4589be9f4bd206379f1cfeb6
Tukks <Giuseppe06@gmail.com>
2026-03-17 17:03:34 +0100

    updat version to 0.0.11

--- _meta.lua
+++ _meta.lua
@@ -2,7 +2,7 @@ local _ = require("gettext")
 return {
     name = "onyx_sync",
     fullname = _("Onyx Progress Sync"),
-    version = "0.0.10",
+    version = "0.0.11",
     description = _("Syncs progress with Onyx library."),
     is_supported = function()
         return require("device"):isAndroid()
